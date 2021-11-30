6bd98c5cabae23cb4690f9d9c7e7227228e71f2a
suhasinisrivastava <81003002+suhasinisrivastava@users.noreply.github.com>
2021-11-30 13:59:19 +0530

    Time: 28 ms (33.33%), Space: 23.4 MB (5.21%) - LeetHub

---
+++ reverse-string/reverse-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        vector<char> a;
+        for(int i=s.size()-1;i>=0;i--){
+            a.push_back(s[i]);
+        }
+        s.clear();
+        s=a;
+        
+        
+    }
+};
