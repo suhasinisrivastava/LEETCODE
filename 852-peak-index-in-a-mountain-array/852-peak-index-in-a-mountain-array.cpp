1be8dda6bd9a259786b1479d23a4434c1d167048
Suhasini Srivastava <81003002+suhasinisrivastava@users.noreply.github.com>
2022-06-15 04:26:12 +0530

    Time: 8 ms (92.14%), Space: 11.6 MB (49.36%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -1,27 +1,41 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int low = 0;
-        int high = arr.size()-1;
-        while(low<=high)
-        {
-		//Always Calculate mid like below to avoid integer overflow
-            int mid = low+(high-low)/2;
-			//If mid=0 then left = INT_MIN
-            int left = mid-1>=0?arr[mid-1]:INT_MIN;
-			//If mid=high, then right = INT_MIN
-            int right = mid+1<=high?arr[mid+1]:INT_MIN;
-			//If element at mid is greater than its left and right then mid is our answer.
-            if(arr[mid]>right && arr[mid]>left)
-                return mid;
-			//If element at mid is smaller than its left element then search in the left part.
-            else if(arr[mid]<left)
-                high = mid-1;
-			//If element at mid is smaller than its right part then search in the right part.
-            else if(arr[mid]<right)
-                low = mid+1;
+        int low=0;
+        int high=arr.size()-1;
+        if(arr.size()==1){
+            return 0;
         }
-		//If  you did not find any peak index then return -1.
-        return -1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(mid>0 && mid<arr.size()-1){
+                if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                    return mid;
+                }
+                else if(arr[mid-1]>arr[mid]){
+                    high=mid-1;
+                }
+                else if(arr[mid+1]>arr[mid]){
+                    low=mid+1;
+                }
+            }
+            else if(mid==0){
+                if(arr[mid]>arr[1]){
+                    return 0 ;
+                }
+                else{
+                    return 1;
+                }
+            }
+            else if(mid==arr.size()-1){
+                if(arr[mid]>arr[arr.size()-2]){
+                    return arr.size()-1;
+                }
+                else{
+                    return arr.size()-2;
+                }
+            }
+        }
+        return 0;
     }
 };
