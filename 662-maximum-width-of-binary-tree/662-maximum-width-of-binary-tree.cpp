ca3f315c16556b27bf633d9ceff4eff32d40833a
Suhasini Srivastava <81003002+suhasinisrivastava@users.noreply.github.com>
2022-06-28 12:48:37 +0530

    Time: 19 ms (27.41%), Space: 17.4 MB (20.59%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -12,45 +12,40 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(root == NULL)
+        if(!root){
             return 0;
-        
-        int res = 1;
-        queue<pair<TreeNode*, int>> q;
-        
-        // I am using intialising list
-        q.push({root, 0});      // also can use make_pair
-        
-        while(!q.empty())
-        {
-            int cnt = q.size();
-            // start is the index of root node for first level
-            int start = q.front().second;
-            int end = q.back().second;
+        }
+        int ans=0;
+        queue<pair<TreeNode*,long long>> q;
+        q.push({root,0});
+        while(!q.empty()){
+            long long minm=q.front().second;
+            long long size=q.size();
             
-            res = max(res,end-start + 1);
-            
-            for(int i = 0; i <cnt; ++i)
-            {
-                pair<TreeNode*, int> p = q.front();
-                // we will use it while inserting it children
-                // left child will be 2 * idx + 1;
-                // right chils will be 2 * idx + 2;
-                int idx = p.second - start;
-                
+            int first,last;
+            for(long long i=0;i<size;i++){
+                long long c=q.front().second-minm;
+                TreeNode* node=q.front().first;
                 q.pop();
                 
-                // if  left child exist
-                if(p.first->left != NULL)
-                    q.push({p.first->left, (long long)2 * idx + 1});
+                if(i==0){
+                    first=c;
+                }
+                if(i==size-1){
+                    last=c;
+                }
+                if(node->left){
+                    q.push({node->left,2*c+1});
+                }
+                if(node->right){
+                    q.push({node->right,2*c+2});
+                }
                 
-                // if right child exist
-                if(p.first->right != NULL)
-                    q.push({p.first->right, (long long) 2 * idx + 2});
             }
+            ans=max(ans,last-first+1);
+            
+            
         }
-        
-        return res;
-        
+        return ans;
     }
 };
