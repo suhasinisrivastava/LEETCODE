d1d3301c7e1130c168617e955a7a01752d8bbb32
Suhasini Srivastava <81003002+suhasinisrivastava@users.noreply.github.com>
2022-08-14 16:26:01 +0530

    Time: 33 ms (22.20%), Space: 5.9 MB (74.58%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
+    bool p(int i, string&s){
+        if(i>=s.size()){
+            return true;
+        }
+        if(s[i]!=s[s.size()-i-1]){
+            return false;
+        }
+        return p(i+1,s);
+    }
     bool isPalindrome(int x) {
-         long long rem=0,r,count=0;
-        int y=x;
-        bool ans=false;
-        if(x<0){
-            ans=false;
-        }
-        else if(x%10==0){
-            ans=false;
-        }
-        else{
-            while(x!=0){
-    	        r=x%10;
-    	       
-    	        rem=rem*10+r;
-    	        
-    	        x/=10;
-    	        
-    	        
-    	    }
-    	    
-        }
-        if(rem==y){
-    	        ans= true;
-    	        
-    	        
-    	    }
-        return ans;
+        string s=to_string(x);
+         return p(0,s);
     }
 };
