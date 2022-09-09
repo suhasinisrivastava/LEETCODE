247774ecbac733159c9bc4b984acf17c916a8988
Suhasini Srivastava <81003002+suhasinisrivastava@users.noreply.github.com>
2022-09-10 03:36:45 +0530

    Time: 11 ms (21.56%), Space: 9.4 MB (78.41%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        bool x=false;
         for(int i=0;i<matrix.size();i++){
             if(target>matrix[i][matrix[i].size()-1]){
                 continue;
             }
             else{
                 for(int j=0;j<matrix[i].size();j++){
-                    if(target==matrix[i][j]){
+                    if(matrix[i][j]==target){
                         return true;
                     }
                 }
             }
         }
-        return false;
+        return x;
     }
 };
