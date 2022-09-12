e50ad2c408fb0cbba6248fb4c827943da3f05398
Suhasini Srivastava <81003002+suhasinisrivastava@users.noreply.github.com>
2022-09-13 04:57:45 +0530

    Time: 55 ms (8.15%), Space: 11.4 MB (19.51%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        unordered_map<char,int>ump;
+        for(int i=0;i<words[0].size();i++){
+            ump[words[0][i]]++;
+        }
+        
+        for(int i=1;i<words.size();i++){
+            unordered_map<char,int>umpt;
+            for(int j=0;j<words[i].size();j++ ){
+                umpt[words[i][j]]++;
+            }
+            
+            for(auto &it:ump){
+                it.second=min(it.second,umpt[it.first]);
+            }
+            for(auto it:ump){
+            cout<<it.first;
+        }
+        }
+        vector<string>v;
+        for(auto &it:ump){
+            while(it.second!=0){
+                string t;
+                t+=it.first;
+                v.push_back(t);
+                ump[it.first]--;
+            }
+        }
+        return v;
+    }
+};
