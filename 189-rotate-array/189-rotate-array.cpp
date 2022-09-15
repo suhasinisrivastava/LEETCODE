228070a381df3a3fd0ed411c6b9ef3439d5e4528
Suhasini Srivastava <81003002+suhasinisrivastava@users.noreply.github.com>
2022-09-15 23:15:49 +0530

    Time: 62 ms (26.62%), Space: 25.1 MB (58.32%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    void reversei(int i,int j, vector<int> &nums){
-        while(i<=j){
+    void rev(int i, int j, vector<int>&nums){
+        while(i<j){
             int temp=nums[i];
             nums[i]=nums[j];
             nums[j]=temp;
@@ -10,17 +10,14 @@ public:
         }
     }
     void rotate(vector<int>& nums, int k) {
-        int temp=k%nums.size();
+        int size=k%nums.size();
         if(k<0){
             k+=nums.size();
         }
-       
-        else if (nums.size()>temp){
-        reversei(nums.size()-temp,nums.size()-1,nums);
-        reversei(0,nums.size()-temp-1,nums);
-        reversei(0,nums.size()-1,nums);
+        else if(nums.size()>size){
+            rev(nums.size()-size,nums.size()-1,nums);
+            rev(0,nums.size()-size-1,nums);
+            rev(0,nums.size()-1,nums);
         }
-        
-        
     }
 };
