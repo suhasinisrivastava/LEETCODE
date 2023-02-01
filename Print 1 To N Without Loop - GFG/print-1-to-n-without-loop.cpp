738249790a61eb7e7ae9ecdfd79eba16d153d8cf
Suhasini Srivastava <81003002+suhasinisrivastava@users.noreply.github.com>
2023-02-01 23:39:21 +0530

    Added solution - LeetHub

--- Print 1 To N Without Loop - GFG/print-1-to-n-without-loop.cpp
+++ Print 1 To N Without Loop - GFG/print-1-to-n-without-loop.cpp
@@ -10,11 +10,11 @@ class Solution{
     void printNos(int N)
     {
         //Your code here
-        
         if(N>0){
             printNos(N-1);
             cout<<N<<" ";
         }
+        
     }
 };
 
